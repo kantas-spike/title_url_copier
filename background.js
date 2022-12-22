06f5279ace5d42978090384f8d80f91f2c0d181c
kanta <101532773+kantas-spike@users.noreply.github.com>
2022-12-22 22:53:03 +0900

    start version control

---
+++ background.js
@@ -0,0 +1,21 @@
+// 参考: https://github.com/GoogleChrome/chrome-extensions-samples
+function copyToClipboard(title, url) {
+    info = `${title}\n\n${url}`
+    navigator.clipboard.writeText(info).then(() => {
+        /* clipboard successfully set */
+        // console.log("success", info)
+      }, () => {
+        /* clipboard write failed */
+        console.log("clipboard write failed!!")
+      });
+  }
+
+  chrome.action.onClicked.addListener((tab) => {
+    if(!tab.url.includes("chrome://")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: copyToClipboard,
+        args: [tab.title, tab.url]
+      });
+    }
+  });
