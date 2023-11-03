931f04843262f16eea12c08cf4c5377c105b2ec8
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-11-03 22:25:29 +0900

    feat: 通知機能を追加

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,18 @@
+const CopyiedURLNotification = "CopiedURL";
+const FailedToCopyURLNotification = "FailedToCopyURL";
+
 function copyToClipboard(title: string | undefined, url: string) {
   const info = `[${title}](${url})`;
-  navigator.clipboard.writeText(info).catch((error) => {
-    /* clipboard write failed */
-    console.error(`clipboard write failed!!: ${error}`);
-  });
+  return navigator.clipboard
+    .writeText(info)
+    .then(() => {
+      return info;
+    })
+    .catch((error) => {
+      /* clipboard write failed */
+      console.error(`clipboard write failed!!: ${error}`);
+      return undefined;
+    });
 }
 
 chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => {
@@ -11,10 +20,33 @@ chrome.action.onClicked.addListener((tab: chrome.tabs.Tab) => {
     return;
   }
   if (!tab.url.includes("chrome://")) {
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: copyToClipboard,
-      args: [tab.title, tab.url],
-    });
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        func: copyToClipboard,
+        args: [tab.title, tab.url],
+      })
+      .then((injectionResults) => {
+        for (const { frameId, result } of injectionResults) {
+          console.log(`frameId: ${frameId}, result: ${result}`);
+          let options: chrome.notifications.NotificationOptions<true>;
+          if (result) {
+            options = {
+              iconUrl: chrome.runtime.getURL("../icon32.png"),
+              message: `クリップボードにコピーしました: \n${result}`,
+              type: "basic",
+              title: "Title and URL Copier",
+            };
+          } else {
+            options = {
+              iconUrl: chrome.runtime.getURL("../icon32.png"),
+              message: `クリップボードにコピーに失敗しました`,
+              type: "basic",
+              title: "Title and URL Copier",
+            };
+          }
+          chrome.notifications.create(options);
+        }
+      });
   }
 });
