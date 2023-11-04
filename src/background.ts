78f9f8a66ce2d2633bff12551b24838d000583c9
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-11-04 12:05:47 +0900

    feat: hashフォーマットに対応

--- src/background.ts
+++ src/background.ts
@@ -4,11 +4,51 @@ const FailedToCopyURLNotification = "FailedToCopyURL";
 function copyToClipboard(
   title: string | undefined,
   url: string,
-  format: string
+  options: { [key: string]: any }
 ) {
-  const info = format
-    .replaceAll("${title}", title ?? "")
-    .replaceAll("${url}", url);
+  function getHashTargetTextFromUrl(url: string) {
+    const targetURL = new URL(url);
+    const hash = targetURL.hash;
+    const hashId = decodeURIComponent(hash.replace("#", ""));
+    const targetFragment = document.getElementById(hashId);
+    return targetFragment?.innerText.replace(/[\n\r]+|[\s]{2,}/g, " ");
+  }
+
+  function truncateString(
+    targetString: string,
+    maxLength = 24,
+    truncatedSuffix = "..."
+  ) {
+    if (targetString.length > maxLength) {
+      const truncatedString = targetString.slice(
+        0,
+        maxLength - truncatedSuffix.length
+      );
+      return truncatedString + truncatedSuffix;
+    } else {
+      return targetString;
+    }
+  }
+
+  function populateTemplate(
+    title: string | undefined,
+    url: string,
+    options: { [key: string]: any }
+  ) {
+    const hashTargetText = getHashTargetTextFromUrl(url);
+    if (hashTargetText && options.useHashFormat) {
+      return options.hashFormat
+        .replaceAll("${title}", title ?? "")
+        .replaceAll("${url}", url)
+        .replaceAll("${hashTitle}", truncateString(hashTargetText));
+    } else {
+      return options.format
+        .replaceAll("${title}", title ?? "")
+        .replaceAll("${url}", url);
+    }
+  }
+
+  const info = populateTemplate(title, url, options);
   return navigator.clipboard
     .writeText(info)
     .then(() => {
@@ -25,20 +65,18 @@ chrome.action.onClicked.addListener(async (tab: chrome.tabs.Tab) => {
   if (tab.url === undefined || tab.id === undefined) {
     return;
   }
-
-  const initStorageCache = await chrome.storage.sync.get();
-  console.log(initStorageCache);
+  const url = new URL(tab.url);
+  const options = await chrome.storage.sync.get();
 
   if (!tab.url.includes("chrome://")) {
     chrome.scripting
       .executeScript({
         target: { tabId: tab.id },
         func: copyToClipboard,
-        args: [tab.title, tab.url, initStorageCache.format],
+        args: [tab.title, tab.url, options],
       })
       .then((injectionResults) => {
         for (const { frameId, result } of injectionResults) {
-          console.log(`frameId: ${frameId}, result: ${result}`);
           let options: chrome.notifications.NotificationOptions<true>;
           if (result) {
             options = {
